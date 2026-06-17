bba40845098f5a8f7c70505a580f83ff64dc8dc6
romakkuzin <romakkuzin4@gmail.com>
2026-06-17 22:50:09 +0500

    fix(config): added whitenoise middleware

--- core/settings.py
+++ core/settings.py
@@ -50,8 +50,11 @@ INSTALLED_APPS = [
 
 TAILWIND_APP_NAME = "theme"
 
+NPM_BIN_PATH = "C:\\Program Files\\nodejs\\npm.cmd"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     
     "django.middleware.common.CommonMiddleware",
@@ -127,3 +130,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
